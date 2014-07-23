e29c40f8ec42745655527454084237b9f28a1ebb
vlpolyansky <vl-polyansky@yandex.ru>
2014-07-23 14:49:21 +0400

    Moved Main.hs test file; added project run configuration in IDEA

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,6 @@
 module Main where
 
-qsort :: Ord a => [a] -> [a]
-qsort [] = []
-qsort (a:as) = qsort left ++ [a] ++ qsort right
-  where (left,right) = (filter (<=a) as, filter (>a) as)
+import Debugger (defaultRunGhc, startCommandLine)
 
 main :: IO ()
-main = print (qsort [8, 4, 0, 3, 1, 23, 11, 18])
-
-foo a = 1
+main = defaultRunGhc startCommandLine
